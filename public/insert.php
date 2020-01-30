bb05137415594257134b3a72cee83d3b42aff3ee
brian2694 <brian2694@hotmail.com>
2020-01-30 18:21:28 -0500

    insert

---
+++ public/insert.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "hola";
+
+$servername = "64.227.53.220";
+$username = "movu";
+$password = "movu";
+$dbname = "movu";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = sprintf("INSERT INTO `movu`.`users` (`name`) VALUES ('%s');", date('h:m:s'));
+$result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//         echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+//     }
+// } else {
+//     echo "0 results";
+// }
+$conn->close();
+?>
