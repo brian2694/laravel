d5671bbb1165fd8ba456bcdbf2e35f46a9c4c18e
brian2694 <brian2694@hotmail.com>
2020-01-30 23:29:56 -0500

    Update PruebaController.php

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class PruebaController extends Controller
 {
+    public function select(){
+        $user = User::select('id', 'name')->first();
+		if($user != null ){
+            return sprintf("Hola bebe: %s, %s", config("app.name"), $user->name);
+        }else{
+            return sprintf("Hola bebe: %s", config("app.name"));
+        }
+	}
+    
     public function index(){
 		if(env("APP_ENV") == "local"){
 			return "local";
